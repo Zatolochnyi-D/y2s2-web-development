f9567aa319ae79d70fc7fee026aa95579dd0ff0b
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-06-06 19:12:44 +0300

    Initialized repository. Created project files.

---
+++ Task1.js
@@ -0,0 +1,14 @@
+// 1
+// Find middle character of the word.
+// User is going to input a word. Your task is to find the middle character of this word.
+// For cancelled input show “Canceled.”
+// If the word’s length is odd - show the middle character.If word’s length is even - show the middle 2 characters.
+// You should validate for empty value and for value with spaces only, in that case show message: “Invalid value”.
+// For user input use “prompt” function. For displaying result use “alert” function.
+
+// Examples:
+//  For user input “test” should return “es”
+//  For user input “character” should return “a”
+//  For user input “B” should return “B”
+//  For user input “” should return “Invalid value”
+// For user input “       “ should return “Invalid value”
---
+++ Task3.js
@@ -0,0 +1,4 @@
+// 4
+
+// Create list on user's names in html document. By click on name display alert 'Hello, {name}'
+console.log("AAA");
