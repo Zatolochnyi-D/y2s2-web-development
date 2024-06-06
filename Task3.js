12415164361442f2154dd46860a669b1fa50914e
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-06-06 19:59:16 +0300

    Completed task3.

--- Task3.js
+++ Task3.js
@@ -1,3 +1,26 @@
 // 4
 
 // Create list on user's names in html document. By click on name display alert 'Hello, {name}'
+class User {
+    constructor(name)
+    {
+        this.name = name
+    }
+}
+
+var users = [
+    new User("Athos"),
+    new User("Porthos"),
+    new User("Aramis"),
+]
+
+var list = document.getElementById("0");
+console.log(list);
+users.forEach(user => {
+    let li = document.createElement('li')
+    li.textContent = user.name
+    li.addEventListener("click", function (e) {
+        alert(e.target.textContent)
+    })
+    list.append(li);
+});
